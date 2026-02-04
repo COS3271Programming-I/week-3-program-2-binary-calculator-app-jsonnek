586bb12e8d79ea84c92698c7f58689fe8ba78c86
jsonnek <jtsonnek@students.unwsp.edu>
2026-02-03 20:48:30 -0600

    Fixed the program to run smoother with fewer lines.

--- Binary Calculator/src/Main.java
+++ Binary Calculator/src/Main.java
@@ -15,29 +15,23 @@ public class Main {
         y = userinput.nextLine();
 
 //      Get User input for operation
-        System.out.println("Select operation (1-6)\n" +
+        System.out.println("Select operation (1-5)\n" +
                 "1:Addition, 2:Subtraction, 3:Multiplication, 4:Division, 5:All operations:");
         int operation = userinput.nextInt();
 //      Perform operation based on user input
-        if (operation == 1) {
+        if (operation == 1 || operation == 5) {
             System.out.println("Sum is : " + Integer.toBinaryString(Integer.parseInt(x,2) + Integer.parseInt(y,2)));
         }
-        else if (operation == 2) {
+        if (operation == 2 || operation == 5) {
             System.out.println("Difference is : " + Integer.toBinaryString(Integer.parseInt(x,2) - Integer.parseInt(y,2)));
         }
-        else if (operation == 3) {
+        if (operation == 3 ||operation == 5) {
             System.out.println("Product is : " + Integer.toBinaryString(Integer.parseInt(x,2) * Integer.parseInt(y,2)));
         }
-        else if (operation == 4) {
+        if (operation == 4 || operation == 5) {
             System.out.println("Quotient is : " + Integer.toBinaryString(Integer.parseInt(x,2) / Integer.parseInt(y,2)));
         }
-        else if (operation == 5) {
-            System.out.println("Sum is : " + Integer.toBinaryString(Integer.parseInt(x,2) + Integer.parseInt(y,2)));
-            System.out.println("Difference is : " + Integer.toBinaryString(Integer.parseInt(x,2) - Integer.parseInt(y,2)));
-            System.out.println("Product is : " + Integer.toBinaryString(Integer.parseInt(x,2) * Integer.parseInt(y,2)));
-            System.out.println("Quotient is : " + Integer.toBinaryString(Integer.parseInt(x,2) / Integer.parseInt(y,2)));
-        }
-        else {
+        if (operation > 5) {
             System.out.println("Invalid operation selected.");
         }
     }
